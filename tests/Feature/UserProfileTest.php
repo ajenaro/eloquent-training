c8bd3c377a39ee620bd39bb9f14545c61cc94571
Antonio Jenaro <antonio.jenaro@gmail.com>
2020-11-26 14:09:51 +0100

    completed lesson 2

---
+++ tests/Feature/UserProfileTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use App\UserProfile;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserProfileTest extends TestCase
+{
+    /** @test */
+    public function a_user_profile_belongs_to_a_user()
+    {
+        $user = factory(User::class)->create();
+        $userProfile = factory(UserProfile::class)->create([
+           'website' => 'https://styde.net',
+           'user_id' => $user->id,
+       ]);
+
+        $this->assertInstanceOf(BelongsTo::class, $userProfile->user());
+        $this->assertInstanceOf(User::class, $userProfile->user);
+        $this->assertTrue($userProfile->user->is($user));
+        $this->assertSame('https://styde.net', $user->profile->website);
+    }
+}
